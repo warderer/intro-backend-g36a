bf4ca625b10c8912a131ed55d347aa2d88e41751
César Guerra <warderer@gmail.com>
2025-02-17 20:02:58 -0600

    ✨ Add express basic server

---
+++ 01-intro-express/index.js
@@ -0,0 +1,19 @@
+// #1 Llamar a la biblioteca express
+const express = require('express')
+
+// #2a Crear una aplicación o instancia de express
+const app = express()
+const port = process.env.PORT || 3000
+
+// #2b Configurar express para que pueda recibir datos de tipo JSON
+app.use(express.json())
+
+// #3 Definir rutas
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+})
+
+// #4 Levantar el servidor
+app.listen(port, () => {
+  console.log(`App listening on port ${port} 🚀`)
+})
